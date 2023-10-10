e274bdc99dd71d93772245fd1894308196c464e4
maiprapawin <maiprapawin@gmail.com>
2023-10-10 17:10:19 +0700

    create UI Login Content

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,11 @@
+import LoginContent from "../features/auth/LoginContent";
+import LoginForm from "../features/auth/LoginForm";
+
 export default function LoginPage() {
-  return <h1>LoginPage</h1>;
+  return (
+    <div className="grid grid-cols-2 w-full h-full">
+      <LoginContent />
+      <LoginForm />
+    </div>
+  );
 }
