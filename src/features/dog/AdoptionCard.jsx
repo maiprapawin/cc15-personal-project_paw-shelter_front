d0966dfd4828076c45433d76c09abc74e041a424
maiprapawin <maiprapawin@gmail.com>
2023-10-18 11:31:44 +0700

    updated how-to-adopt menu path

---
+++ src/features/dog/AdoptionCard.jsx
@@ -0,0 +1,12 @@
+export default function AdoptionCard() {
+  return (
+    <div>
+      <h1>01</h1>
+      <h3>Look for a suitable dog</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        vitae cumque a quaerat sequi magnam ullam aliquam modi mollitia illum!
+      </p>
+    </div>
+  );
+}
--- src/layout/Menu.jsx
+++ src/layout/Menu.jsx
@@ -2,7 +2,7 @@ import MenuItem from "./MenuItem";
 
 const menus = [
   { id: 1, to: "/", name: "Home" },
-  { id: 2, to: "how-to-adopt", name: "How to Adopt" },
+  { id: 2, to: "/how-to-adopt", name: "How to Adopt" },
 ];
 
 export default function Menu() {
